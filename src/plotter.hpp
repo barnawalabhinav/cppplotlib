a7e7438939570ef4a684cc7240c396a36a10fa9d
Abhinav Barnawal <abhinavbarnawal0123@gmail.com>
2024-07-11 18:15:27 +0530

    Update plotter.hpp

--- src/plotter.hpp
+++ src/plotter.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <assert.h>
 #include <fstream>
+#include <unistd.h>
 
 class Plotter
 {
@@ -22,15 +23,22 @@ public:
 
         if (gnuplotPipe)
             fprintf(gnuplotPipe, "set terminal pngcairo enhanced font ',%d' size %d, %d\n", fontSize, size_x, size_y);
+        else
+            std::cerr << "Could not set up pipe with gnuplot" << std::endl;
     }
 
     inline virtual ~Plotter()
     {
         if (gnuplotPipe)
+        {
             pclose(gnuplotPipe);
 
-        // for (int i = 0; i < cnt_lines; i++)
-        //     std::remove((std::to_string(i) + ".dat").c_str());
+            for (int i = 0; i < cnt_files; i++)
+            {
+                unlink((std::to_string(i) + ".dat").c_str());
+                // std::remove((std::to_string(i) + ".dat").c_str());
+            }
+        }
     }
 
     inline void reset_plot(int size_x = 1200, int size_y = 900, int fontSize = 20)
@@ -62,17 +70,8 @@ public:
         fprintf(gnuplotPipe, "set style data boxplot\n");
         fprintf(gnuplotPipe, "set style boxplot outliers pointtype 7\n");
 
-        std::string filename = std::to_string(cnt_lines) + "box.dat";
+        std::string filename = std::to_string(cnt_files) + ".dat";
         std::ofstream fout(filename);
-
-        std::string xtics_cmd = "set xtics (";
-        for (int i = 0; i < x.size() - 1; i++)
-            xtics_cmd += "\"" + x[i] + "\" " + std::to_string(i + 1) + ", ";
-
-        xtics_cmd += "\"" + x.back() + "\" " + std::to_string(x.size()) + ")\n";
-
-        if (show_xticks)
-            fprintf(gnuplotPipe, "%s", xtics_cmd.c_str());
 
         if (color != "auto")
             for (int i = 0; i < y.size(); i++)
@@ -91,16 +90,14 @@ public:
 
         fprintf(gnuplotPipe, "\n");
         fprintf(gnuplotPipe, "unset style boxplot\n");
-        cnt_lines++;
+        cnt_files++;
     }
 
-    inline void createPlot(const std::vector<double> &x, const std::vector<double> &y, const char *line_title = "", const char *linecolor = "auto", const int shift = 0.0)
+    template <typename T1, typename T2>
+    inline void createPlot(const std::vector<T1> &x, const std::vector<T2> &y, const char *line_title = "", const char *linecolor = "auto", const T1 shift = static_cast<T1>(0))
     {
-        if (x.size() <= 1 || y.size() <= 1)
-            return;
-
         fprintf(gnuplotPipe, "plot ");
-        std::string filename = std::to_string(cnt_lines) + ".dat";
+        std::string filename = std::to_string(cnt_files) + ".dat";
         std::ofstream fout(filename);
 
         for (int i = 0; i < x.size(); i++)
@@ -114,15 +111,31 @@ public:
         else
             fprintf(gnuplotPipe, "\"%s\" using 1:2 smooth unique with lines linecolor '%s' title '%s'", filename.c_str(), linecolor, line_title);
 
-        cnt_lines++;
+        cnt_files++;
     }
 
-    inline void addPlot(const std::vector<double> &x, const std::vector<double> &y, const char *line_title, const char *linecolor = "auto", int shift = 0.0)
+    template <typename T2>
+    inline void createPlot(const std::vector<T2> &y, const char *line_title = "", const char *linecolor = "auto")
     {
-        if (x.size() <= 1 || y.size() <= 1)
-            return;
+        fprintf(gnuplotPipe, "plot ");
+        std::string filename = std::to_string(cnt_files) + ".dat";
+        std::ofstream fout(filename);
 
-        std::string filename = std::to_string(cnt_lines) + ".dat";
+        for (int i = 0; i < y.size(); i++)
+            fout << i << " " << y[i] << "\n";
+
+        if (linecolor == "auto")
+            fprintf(gnuplotPipe, "\"%s\" using 1:2 smooth unique with lines title '%s'", filename.c_str(), line_title);
+        else
+            fprintf(gnuplotPipe, "\"%s\" using 1:2 smooth unique with lines linecolor '%s' title '%s'", filename.c_str(), linecolor, line_title);
+
+        cnt_files++;
+    }
+
+    template <typename T1, typename T2>
+    inline void addPlot(const std::vector<T1> &x, const std::vector<T2> &y, const char *line_title, const char *linecolor = "auto", const T1 shift = static_cast<T1>(0))
+    {
+        std::string filename = std::to_string(cnt_files) + ".dat";
         std::ofstream fout(filename);
 
         for (int i = 0; i < x.size(); i++)
@@ -136,7 +149,24 @@ public:
         else
             fprintf(gnuplotPipe, ", \"%s\" using 1:2 smooth unique with lines linecolor '%s' title '%s'", filename.c_str(), linecolor, line_title);
 
-        cnt_lines++;
+        cnt_files++;
+    }
+
+    template <typename T2>
+    inline void addPlot(const std::vector<T2> &y, const char *line_title, const char *linecolor = "auto")
+    {
+        std::string filename = std::to_string(cnt_files) + ".dat";
+        std::ofstream fout(filename);
+
+        for (int i = 0; i < y.size(); i++)
+            fout << i << " " << y[i] << "\n";
+
+        if (linecolor == "auto")
+            fprintf(gnuplotPipe, ", \"%s\" using 1:2 smooth unique with lines title '%s'", filename.c_str(), line_title);
+        else
+            fprintf(gnuplotPipe, ", \"%s\" using 1:2 smooth unique with lines linecolor '%s' title '%s'", filename.c_str(), linecolor, line_title);
+
+        cnt_files++;
     }
 
     void set_xlabel(const char *label)
@@ -163,7 +193,29 @@ public:
             fprintf(gnuplotPipe, "\nset output '%s'\n", savePath);
     }
 
+    template <typename T>
+    void set_xtics(const std::vector<T> &x)
+    {
+        std::string xtics_cmd = "set xtics (";
+        for (int i = 0; i < x.size() - 1; i++)
+            xtics_cmd += "\"" + x[i] + "\" " + std::to_string(i) + ", ";
+
+        xtics_cmd += "\"" + x.back() + "\" " + std::to_string(x.size() - 1) + ")\n";
+        fprintf(gnuplotPipe, "%s", xtics_cmd.c_str());
+    }
+
+    template <typename T>
+    void set_ytics(const std::vector<T> &y)
+    {
+        std::string ytics_cmd = "set ytics (";
+        for (int i = 0; i < y.size() - 1; i++)
+            ytics_cmd += "\"" + y[i] + "\" " + std::to_string(i) + ", ";
+
+        ytics_cmd += "\"" + y.back() + "\" " + std::to_string(y.size() - 1) + ")\n";
+        fprintf(gnuplotPipe, "%s", ytics_cmd.c_str());
+    }
+
 private:
     FILE *gnuplotPipe;
-    int cnt_lines = 0;
+    int cnt_files = 0;
 };
